b7fe3883368bab1a14c125f23c1dc697648ccf44
Your NameThu <linhlinhlinh6767@gmail.com>
2022-07-06 14:38:53 +0700

    add home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header/Header";
+import HomePages from "./home/HomePages";
+
 
 function App() {
     return (
@@ -8,6 +10,12 @@ function App() {
             <Router>
                 <Header></Header>
                 <Routes>
+                    <Route exact path="/" component={HomePages} />
+                    {/* <Route
+                        path="/singlepage/:id"
+                        component={SinglePage}
+                        exact
+                    /> */}
                     {/* <Route path="/about">
             <About />
           </Route>
